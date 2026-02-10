97dda465764decd808b8671f25ec66fbb3cbe1c7
LCkatariya <lalchandkatariya91@gmail.com>
2026-02-10 19:51:59 +0530

    update

--- app/layout/AppHeader.tsx
+++ app/layout/AppHeader.tsx
@@ -84,20 +84,22 @@ const AppHeader: React.FC = () => {
           </button>
 
           <Link href="/" className="lg:hidden">
+
             <Image
-              width={154}
-              height={32}
-              className="dark:hidden"
-              src="./images/owner.svg"
+            className="w-11 h-11 rounded-full "
+              width={44}
+              height={44}
+              src="/images/owner.jpg"
               alt="Logo"
             />
-            <Image
+       
+            {/* <Image
               width={154}
               height={32}
               className="hidden dark:block"
               src="./images/logo/logo-dark.svg"
               alt="Logo"
-            />
+            /> */}
           </Link>
 
           <button
