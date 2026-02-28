f0f6ea1e9cae2a286980b5186c67ba01561fa4a0
LCkatariya <lalchandkatariya91@gmail.com>
2026-02-28 23:28:15 +0530

    update

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL:
-    "https://vendingmac-f5e47-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "https://otp-vp-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
